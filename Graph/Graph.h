9747578cc23cd1ed1693180a40c6c7e66944987e
Arsen Arutyunyan <arutyunyanas@sgu.ru>
2015-03-27 12:20:47 +0400

    Init task6

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -19,23 +19,25 @@ struct Exeption {
 class Graph {
 public:
     Graph() { };
-	bool exists(int id);			 					// проверить на наличие вершины в графе
-    void addVertex(int id);		 						// добавить вершину 
-    void addEdge(int from, int to, int weight); 		// добавить дугу
-    int getWeight(int from, int to);			 		// получить вес ребра
-    int size();									 		// размерность графа
-    void show();								 		// отобразить граф
-	Vertex* getVertex(int id);		 					// доступ к вершине по имени
-    Vertex* operator[](int id);	 						// доступ к вершине по имени
-	void removeVertex(int removable_id);		 		// удалить вершину
-	void removeEdge(int from_, int to_);		 		// удалить дугу
-	int indegree(int id);						 		// полустепень захода
-	void scan(string fileName);					  		// считать граф из файла
-	vector<Vertex*> vertices();						    // возвращает список смежности
-	void Graph::dfs(int i, vector<bool> &used);			// обход графа в глубину
-	bool isConnected();									// проверка графа на связность
-	int edgesNumber();									// количество ребер
-	bool isTree();										// проверка является ли граф деревом
+	bool exists(int id);			 						// проверить на наличие вершины в графе
+    void addVertex(int id);		 							// добавить вершину 
+    void addEdge(int from, int to, int weight); 			// добавить дугу
+    int getWeight(int from, int to);			 			// получить вес ребра
+    int size();									 			// размерность графа
+    void show();								 			// отобразить граф
+	Vertex* getVertex(int id);		 						// доступ к вершине по имени
+    Vertex* operator[](int id);	 							// доступ к вершине по имени
+	void removeVertex(int removable_id);		 			// удалить вершину
+	void removeEdge(int from_, int to_);		 			// удалить дугу
+	int indegree(int id);						 			// полустепень захода
+	void scan(string fileName);					  			// считать граф из файла
+	vector<Vertex*> vertices();								// возвращает список смежности
+	void dfs(int i, vector<bool> &used);					// обход графа в глубину
+	void dfs(int i, vector<bool> &used, vector<char> cycle);// обход графа в глубину (цветные вершины)
+	void bfs(int i);										// обход графа в ширину
+	bool isConnected();										// проверка графа на связность
+	int edgesNumber();										// количество ребер
+	bool isTree();											// проверка является ли граф деревом
 
 protected:
     vector<Vertex*> vertexList;
